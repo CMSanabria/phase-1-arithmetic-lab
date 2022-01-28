51141dbb7e900c29cfcd11ac9e422b33ad64bf05
Carlos Sanabria <Sanabria26mc@gmail.com>
2022-01-28 12:11:49 -0800

    commit tree 196876dfcf2ee53c26fcdd3654a85c48d86430cc

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100);
+
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4;
+
+const max = Math.max(1, 2, 5, 10, 20);
+
+
